0cda183baa47e5164d365e772489dfc0d7d8e816
crystal113126 <97917469+crystal113126@users.noreply.github.com>
2023-08-10 16:35:53 -0400

    Frontend using React

---
+++ React/movie-gold-v1/src/components/Layout.js
@@ -0,0 +1,14 @@
+import { Outlet} from "react-router-dom";
+
+import React from "react";
+
+const Layout = () => {
+    return (
+      <>
+       <Outlet />
+      </>
+            
+    )
+};
+
+export default Layout
